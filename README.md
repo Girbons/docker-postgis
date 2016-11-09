597687986985c287cca827ccb0c82cf57aa7979c
Federico Frenguelli <synasius@gmail.com>
2016-11-09 11:13:51 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# PostGIS - dockerfiles
+
+Dockerfile to build a postgresql image with postgis functionalities enabled.
+
+Based on library/postgresql:9.6
+
+Pinned to
+
+```
+ postgresql: 9.6
+ postgis:    2.3.0+dfsg-2.pgdg80+1
+```
---
+++ initdb-postgis.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+# Load PostGIS into $POSTGRES_DB
+echo "Loading PostGIS extensions into ${POSTGRES_DB}"
+"${psql[@]}" --dbname="${POSTGRES_DB}" --user="${POSTGRES_USER}" <<- 'EOSQL'
+    CREATE EXTENSION IF NOT EXISTS postgis;
+    CREATE EXTENSION IF NOT EXISTS postgis_topology;
+    CREATE EXTENSION IF NOT EXISTS fuzzystrmatch;
+    CREATE EXTENSION IF NOT EXISTS postgis_tiger_geocoder;
+EOSQL
